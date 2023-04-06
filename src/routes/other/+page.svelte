932d0aa265f712aeb183cfec01b83c40ba4d6a2f
philanderson888 <philanderson888@hotmail.com>
2023-04-06 06:13:03 +0100

    initial commit

---
+++ src/routes/other/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>Other</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<h1 class="dark:text-white dark-dark-900">Other</h1>
+
+<h1 class="dark:text-white dark-dark-900">Other</h1>
+
+<p>This is some text</p>
+
+
